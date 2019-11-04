92e8cba31576be4e736c710b25f5d8323dbfa090
haysmith <haysmith@umich.edu>
2019-11-04 18:53:03 -0500

    Fixed bug in test_rpn

--- test_rpn.py
+++ test_rpn.py
@@ -4,14 +4,14 @@ import rpn
 
 class TestBasics(unittest.TestCase):
     def test_add(self):
-        result = rpn.calculate("1 1 +")
+        result = rpn.calculate("1 1 +", 0)
         self.assertEqual(2, result)
     def test_subtract(self):
-        result = rpn.calculate("5 3 -")
+        result = rpn.calculate("5 3 -", 0)
         self.assertEqual(2, result)
     def test_multiply(self):
-        result = rpn.calculate("5 3 *")
+        result = rpn.calculate("5 3 *", 0)
         self.assertEqual(15, result)
     def test_divide(self):
-        result = rpn.calculate("6 3 /")
+        result = rpn.calculate("6 3 /", 0)
         self.assertEqual(2, result)
