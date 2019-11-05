ce0cf621cbb1b17ae6f60d36e2f9ebff98efc808
haysmith <haysmith@umich.edu>
2019-11-04 20:07:44 -0500

    added colored text

--- rpn.py
+++ rpn.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
 import operator
+import readline
+from colored import fg, bg, attr
 
+
+color = [fg('114'), fg('220'), fg('205'), fg('99')]
 
 operators = {
     '+': operator.add,
@@ -12,12 +16,11 @@ operators = {
 }
 
 def print_help():
-    print("Accepted Format: arg1 arg2 operator")
-    print("Supported Operators:")
-    op_string = ""
+    print("Accepted Format: " + color[0] + "arg1 " + color[1] + "arg2 " + color[2] + "operator" + attr('reset'))
+    op_string = color[2]
     for item in operators.keys():
         op_string += item + " "
-    print(op_string)
+    print("Supported Operators: " + op_string + attr('reset'))
 
 def calculate(myarg, prev_result):
     stack = list()
@@ -36,19 +39,24 @@ def calculate(myarg, prev_result):
             arg1 = stack.pop()
             result = function(arg1, arg2)
             stack.append(result)
-        print(stack)
+        #print(stack)
     if len(stack) != 1:
         raise TypeError("Too many parameters")
+    color_str = ""
+    for i, token in enumerate(myarg.split()):
+        color_str += color[i] + token + " "
+    color_str += attr('reset')
+    print(color_str)
     return stack.pop()
 
 def main():
     result = 0
     while True:
         try:
-            result = calculate(raw_input("rpn calc> "), result)
+            result = calculate(input("rpn calc> "), result)
         except ValueError:
             continue
-        print("Result: ", result)
+        print("Result: ", color[3] + str(result) + attr('reset'))
 
 if __name__ == '__main__':
     main()
